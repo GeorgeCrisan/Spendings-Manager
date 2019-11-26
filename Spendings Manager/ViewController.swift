54810ad493d43c4bfe91ede917f9519a97006a83
GeorgeCrisan <georgerdp@gmail.com>
2019-11-26 23:32:16 +0000

    Initial Commit

---
+++ Spendings Manager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spendings Manager
+//
+//  Created by George Crisan on 26/11/2019.
+//  Copyright © 2019 George Crisan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
